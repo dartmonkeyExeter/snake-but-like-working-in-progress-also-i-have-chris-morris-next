11efaef8dfe88d289e58b5fa87d7e140c5ad34f1
aaron <146187041+dartmonkeyExeter@users.noreply.github.com>
2023-11-21 22:02:49 +0000

    it works

--- main.py
+++ main.py
@@ -1,64 +1,89 @@
 import random, time, os
-          
-#Initialise maze, bombs, laser source and exit gate
-maze = [[" -","--","--","--","--","--","--","--","--","--","--","- "],
-        [" |","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","| "],
-        [" |","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","| "],
-        [" |","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","| "],
-        [" |","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","| "],
-        [" |","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","| "],
-        [" |","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","| "],
-        [" |","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","| "],
-        [" |","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","| "],
-        [" |","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","| "],
-        [" |","  ","  ","  ","  ","  ","  ","  ","  ","  ","  ","| "],
-        [" -","--","--","--","--","--","--","--","--","--","--","- "]
-       ]
+
+maze = [["--" for i in range(0, 12)]]
+maze += [[" " for i in range(0, 24)] for j in range(12)]
+maze += [["--" for i in range(0, 12)]]
+for jidx, row in enumerate(maze):
+    for idx, cell in enumerate(row):
+        if cell == " " and (idx == 0 or idx == 23):
+            maze[jidx][idx] = "|"
+
+def place_apple():
+    while True:
+        row = random.randint(1, 10)
+        col = random.randint(0, 23)
+        if maze[row][col] == " ":
+            maze[row][col] = "*"
+            break
+        else:
+            continue
 
 def drawMaze():
+    for i in maze:
+        print("".join(i))
 
-  for i in range(0,12):
-    line = ""
-    for j in range(0,12):
-       line = line + maze[i][j]
-    if i>0 and i<10:
-      print(" " + line)
-    elif i==0 or i==11:
-      print("  " + line)
-    elif i==10:
-      print(" " + line)
+directions_dict = {
+    "w": "up",
+    "a": "left",
+    "s": "down",
+    "d": "right",
+}
+directions_list = ["up", "left", "down", "right"]
 
 def shoot():
-  global direction
-  state="shooting"
-  source = (5,0)
-  row = source[0]
-  col = source[1]
-  direction="right"
-  length = 3
+    global direction
+    state = "shooting"
+    direction = "right"
+    source = (5, 0)
+    row = source[0]
+    col = source[1]
+    length = 5
+    old_parts = []
+    place_apple()
+    while state == "shooting":
+        os.system("cls")
+        if direction == "right":
+            col = col + 1
+        elif direction == "left":
+            col = col - 1
+        elif direction == "up":
+            row = row - 1
+        elif direction == "down":
+            row = row + 1
 
-  while state=="shooting":
-      os.system("cls")
-      if direction=="right":
-        col = col + 1
-      ##Add code here to allow laser to move in all 4 directions  
-      elif direction=="left":
-        col = col - 1
-      elif direction=="up":
-        row = row + 1
-      elif direction=="down":
-        row = row - 1
-        
-      if maze[row][col]=="  ":
-        print("")
-        maze[row][col]="++"
-        # code here for deleting old snake parts
-      elif maze[row][col]==" |" or maze[row][col]=="| " or maze[row][col]=="--":
-        print("snake dead")
-        state="lost"
+        if maze[row][col] == " ":
+            print("")
+            maze[row][col] = "+"
+            old_parts.append([row, col])
+            check = len(old_parts) - length
+            if check != 0:
+                for i in range(check):
+                    maze[old_parts[0][0]][old_parts[0][1]] = " "
+                    old_parts.pop(0)
+        elif maze[row][col] == "|" or maze[row][col] == "|" or maze[row][col] == "--" or maze[row][col] == "+":
+            print("snake dead")
+            state = "lost"
+        elif maze[row][col] == "*":
+            maze[row][col] = "+"
+            length += 1
+            old_parts.append([row, col])
+            check = len(old_parts) - length
+            if check != 0:
+                for i in range(check):
+                    maze[old_parts[0][0]][old_parts[0][1]] = " "
+                    old_parts.pop(0)
+            place_apple()
 
-      drawMaze()
-      time.sleep(0.2)
-
+        drawMaze()
+        where_to_go = input()
+        try:
+            next_direction = directions_dict[where_to_go]
+            dir_idx = directions_list.index(next_direction)
+            if dir_idx + 2 == directions_list.index(direction) or dir_idx - 2 == directions_list.index(direction):
+                pass
+            else:
+                direction = next_direction
+        except KeyError:
+            pass
 
 shoot()
